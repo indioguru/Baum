49c663c6f0ea4bd314a3b27f2f3510a6601fb48d
kevin4809 <kevinedk33@gmail.com>
2022-12-07 15:14:48 -0500

    detail
    detail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,24 @@ const Footer = () => {
       <div className="footer_container containerAll">
         <div className="content_rights">
           <a href="#/">Políticas de privacidad </a>
-          <a href="#/">© 2023 BaumFest</a>
+          <p>© 2023 BaumFest</p>
         </div>
 
         <div className="content_icons">
           <div className="instagram">
             <a target="_blank" href="/#">
-            <div className="ins"></div>
+              <div className="ins"></div>
             </a>
           </div>
           <div className="facebook">
             <a target="_blank" href="/#">
-            <div className="fa"></div>
+              <div className="fa"></div>
             </a>
           </div>
 
           <div className="spotify">
             <a target="_blank" href="/#">
-             <div className="spo"></div>
+              <div className="spo"></div>
             </a>
           </div>
         </div>
