84f79bb93d03f8744b007b4f9531bc9d4a52f5ec
kevin4809 <kevinedk33@gmail.com>
2023-01-16 09:24:11 -0500

    1
    1

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="spinner">
+        <span>C</span>
+        <span>A</span>
+        <span>R</span>
+        <span>G</span>
+        <span>A</span>
+        <span>N</span>
+        <span>D</span>
+        <span>O</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Loader from "../components/Loader";
 import HomeIntro from "./HomeIntro";
 // import HomeLineup from "./HomeLineup";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  window.addEventListener("load", () => {
+    setIsLoading(false);
+  });
+
   return (
     <div className="home ">
+      {isLoading && <Loader />}
       <video className="video" autoPlay muted loop id="myVideo">
         <source src="/assets/introbaum3.mp4" type="video/mp4" />
       </video>
       <div className="home_container containerAll">
-        <HomeIntro />
+        {!isLoading && <HomeIntro />}
         {/* <HomeLineup /> */}
       </div>
     </div>
